1f8eca0a30064206e34bcdce2b51d5d8132edec9
robroplol <robrop92@gmail.com>
2022-03-29 16:41:15 -0400

    added fonts and fixed some layout stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Gotham", "Roboto", "Noto Sans", "Helvetica", "Calibri"],
+      },
+    },
   },
   plugins: [],
 };
