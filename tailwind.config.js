28a613b75ebbdde0d5427d1fb1b0daa46a09585d
robroplol <robrop92@gmail.com>
2022-03-30 13:22:25 -0400

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    colors: {
-      white: "#ffffff",
-      "deep-teal": "#093c44",
-      "dark-teal": "#115e6e",
-      "light-teal": "#2f8d98",
-      green: "#b5cd34",
-      orange: "#f7941e",
-      "dark-gray": "#414042",
-      "light-gray": "#6d6e71",
-      "ada-green": "#6a7f17",
-      "ada-orange": "#ac6610",
-    },
     extend: {
       fontFamily: {
         body: ["Gotham", "Roboto", "Noto Sans", "Helvetica", "Calibri"],
+      },
+      colors: {
+        "deep-teal": "#093c44",
+        "dark-teal": "#115e6e",
+        "light-teal": "#2f8d98",
+        green: "#b5cd34",
+        orange: "#f7941e",
+        "dark-gray": "#414042",
+        "light-gray": "#6d6e71",
+        "ada-green": "#6a7f17",
+        "ada-orange": "#ac6610",
       },
     },
   },
